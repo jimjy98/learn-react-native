1e02c05116b75e1804b203d621bef9eb0dbc0147
Jimmy <jimjy98@gmail.com>
2020-05-03 15:25:59 -0400

    add color and counter

---
+++ new2020/rn-starter/src/components/ColorCounter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
+
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+    return (
+        <View>
+
+            <Text>{color}</Text>
+
+            <Button
+                onPress={() => onIncrease()}
+                title={`Increase ${color}`}
+            />
+
+            <Button
+                onPress={() => onDecrease()}
+                title={`Decrease ${color}`}
+            />
+            
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
