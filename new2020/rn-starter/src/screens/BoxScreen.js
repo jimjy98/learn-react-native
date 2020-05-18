05c57534f3a95c5776a6f66028bafcb707c70919
Jimmy <jimjy98@gmail.com>
2020-05-18 12:30:12 -0400

    added css styling with flex

--- new2020/rn-starter/src/screens/BoxScreen.js
+++ new2020/rn-starter/src/screens/BoxScreen.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-    return(
-        <View>
-            <Text>Box Screen</Text>
+    return (
+        <View style={styles.viewStyle}>
+            <View style={styles.boxOneStyle} />
+            <View style={styles.boxTwoStyle} />
+            <View style={styles.boxThreeStyle} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        height: 200,
+        alignItems: 'flex-start', //default: stretch, aligning items horizontally
+        flexDirection: 'row', //default: column.
+        justifyContent: 'space-between' //opposite of align-items (vertically), default: flex-start
+    },
+    boxOneStyle: {
+        backgroundColor: 'red',
+        height: 50,
+        width: 50
+    },
+    boxTwoStyle: {
+        backgroundColor: 'green',
+        height: 50,
+        width: 50,
+        marginTop: 50
+    },
+    boxThreeStyle: {
+        backgroundColor: 'purple',
+        height: 50,
+        width: 50 
+    }
+});
 
 export default BoxScreen;
